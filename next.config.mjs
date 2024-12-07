8a885bc130f3fa8edb71178c9bf3a8432c646bda
Staywithtatar <154976428+Staywithtatar@users.noreply.github.com>
2024-12-07 14:04:07 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = { images: {
+    domains: ["raw.githubusercontent.com", "picsum.photos", "rickandmortyapi.com"]
+} };
 
 export default nextConfig;
